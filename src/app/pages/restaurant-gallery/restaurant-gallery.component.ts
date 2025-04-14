f6384a7e12b98974033814c15f489551db387fcd
deepakoraon19 <deepak.19ranchi@gmail.com>
2025-04-14 16:30:41 +0530

    Infinite loading on scroll

--- src/app/pages/restaurant-gallery/restaurant-gallery.component.ts
+++ src/app/pages/restaurant-gallery/restaurant-gallery.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RestaurantTileComponent } from "../../components/restaurant-tile/restaurant-tile.component";
+import { RestaurantTileComponent } from '../../components/restaurant-tile/restaurant-tile.component';
 import { Environment } from 'src/app/environments/environment';
 import { DataService } from 'src/app/services/data.service';
 import { Restaurant } from 'src/app/models/restaurants';
@@ -10,17 +10,39 @@ import { Restaurant } from 'src/app/models/restaurants';
   standalone: true,
   imports: [CommonModule, RestaurantTileComponent],
   templateUrl: './restaurant-gallery.component.html',
-  styleUrls: ['./restaurant-gallery.component.css']
+  styleUrls: ['./restaurant-gallery.component.css'],
 })
 export class RestaurantGalleryComponent implements OnInit {
-  constructor(private dataSvc : DataService) {}
+  constructor(private dataSvc: DataService) {}
   data: Restaurant[] = [];
+  page = 0;
+  loadingData = false;
+
   ngOnInit(): void {
+    this.fetchRestaurant();
+  }
+
+  private fetchRestaurant() {
+    this.loadingData = true
     this.dataSvc
-      .getRestaurants(`${Environment.API_URL}/api/Restaurant?page=0`)
+      .getRestaurants(`${Environment.API_URL}/api/Restaurant?page=${this.page}`)
       .subscribe((p) => {
-        this.data = p;
+        this.data = [...this.data, ...p];
+        this.loadingData = false
       });
   }
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const pageHeight = document.body.offsetHeight;
+
+    const buffer = 250;
+    const atBottom = scrollPosition >= pageHeight - buffer;
+
+    if (atBottom && !this.loadingData) {
+      this.page++;
+      this.fetchRestaurant();
+    }
+  }
 }
