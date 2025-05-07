f69a790fbfcdb1d6dca428edcde0bc5e61ea425c
deepakoraon19 <deepak.19ranchi@gmail.com>
2025-05-07 15:29:40 +0530

    build fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { RestaurantGalleryComponent } from './pages/restaurant-gallery/restaurant-gallery.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { RestaurantMenuComponent } from './pages/restaurant-menu/restaurant-menu.component';
-import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
-  {path: "login",loadChildren: () => import('./pages/login/login.component').then(m => m.LoginComponent)},
-  {path: "home",loadChildren: () => import('./pages/restaurant-gallery/restaurant-gallery.component').then(m => m.RestaurantGalleryComponent)},
-  {path: "sign-up", loadChildren:()=>import("./pages/sign-up/sign-up.component").then(m => m.SignUpComponent)},
-  {path: "restaurant/:id",loadChildren:()=>import("./pages/restaurant-menu/restaurant-menu.component").then(m => m.RestaurantMenuComponent)},
-  {path: "cart",loadChildren:()=>import("./pages/cart/cart.component").then(m => m.CartComponent)},
-  {path: "", redirectTo:'login', pathMatch:"full"},
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/restaurant-gallery/restaurant-gallery.component').then(
+        (m) => m.RestaurantGalleryComponent
+      ),
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () =>
+      import('./pages/sign-up/sign-up.component').then(
+        (m) => m.SignUpComponent
+      ),
+  },
+  {
+    path: 'restaurant/:id',
+    loadComponent: () =>
+      import('./pages/restaurant-menu/restaurant-menu.component').then(
+        (m) => m.RestaurantMenuComponent
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./pages/cart/cart.component').then((m) => m.CartComponent),
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
